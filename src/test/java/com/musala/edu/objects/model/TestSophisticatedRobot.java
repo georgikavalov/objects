4ca70822f59e226c2e71bdbaccbb26144041f048
georgi.kavalov <georgi.kavalov@GeorgiKa-T580.musala.com>
2018-07-27 13:00:13 +0300

    Task three is complete - public method to draw a square, that takes start coordinates and size of square side. Added private methods to point a robot in specific direction and go to a coordinate.

--- src/test/java/com/musala/edu/objects/model/TestSophisticatedRobot.java
+++ src/test/java/com/musala/edu/objects/model/TestSophisticatedRobot.java
@@ -1,6 +1,7 @@
 package com.musala.edu.objects.model;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,20 +16,24 @@ import kareltherobot.World;
  * {@link SophisticatedRobot}
  */
 public class TestSophisticatedRobot extends KJRTest {
+	private static final int PACES = 1;
+	private static final int STREETS = 30;
+	private static final int AVENUES = 30;
 	private static final int START_AVENUE = 2;
 	private static final int START_STREET = 2;
 	private SophisticatedRobot batka;
 
 	@Before
 	public void setup() {
-		batka = new SophisticatedRobot(START_STREET, START_AVENUE, Directions.North, Color.ORANGE);
+		World.setSize(STREETS, AVENUES);
 		World.setDelay(20);
+		batka = new SophisticatedRobot(START_STREET, START_AVENUE, Directions.North, Color.ORANGE);
 		World.setVisible();
 	}
 
 	@After
 	public void cleanup() {
-		World.resetRobots();
+		World.reset();
 	}
 
 	@Test
@@ -39,10 +44,21 @@ public class TestSophisticatedRobot extends KJRTest {
 
 	@Test
 	public void testBasicMoves() {
-		batka.circleCounterClockwise();
+		batka.circleCounterClockwise(PACES, false);
 		assertAt(batka, START_STREET, START_AVENUE);
 		batka.turnRight();
-		batka.circleClockwise();
+		batka.circleClockwise(PACES, false);
 		assertAt(batka, START_STREET, START_AVENUE);
+	}
+
+	@Test
+	public void testDrawingSquare() {
+		final int squareSize = new Random().nextInt(STREETS - START_AVENUE);
+		batka.drawSquare(START_STREET + 2, START_AVENUE + 4, squareSize);
+		assertBeepersAt(START_AVENUE + 4, START_STREET + 2);// corner A
+		assertBeepersAt(START_AVENUE + 4, START_STREET + 2 + squareSize);// corner B
+		assertBeepersAt(START_AVENUE + 4 + squareSize, START_STREET + 2 + squareSize); // corner C
+		assertBeepersAt(START_AVENUE + 4 + squareSize, START_STREET + 2); // corner D
+		assertBeepersInWorld(4 * squareSize);
 	}
 }
